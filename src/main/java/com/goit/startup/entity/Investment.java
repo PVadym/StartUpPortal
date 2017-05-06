d8fc8b89c1ab8ff2e2777707a8055b59b370e3b9
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2017-05-06 13:37:45 +0300

    Investment with Builder

--- src/main/java/com/goit/startup/entity/Investment.java
+++ src/main/java/com/goit/startup/entity/Investment.java
@@ -10,4 +10,59 @@ public class Investment implements Serializable {
     private int amount;
     private User user;
     private Startup startup;
+
+    private Investment() {
+    }
+
+    public static Builder newBuilder() {
+        return new Investment().new Builder();
+    }
+
+    public class Builder {
+
+        private Builder() {
+            // private constructor
+        }
+
+        public Builder setId(long id) {
+            Investment.this.id = id;
+
+            return this;
+        }
+
+        public Builder setAmount(int amount) {
+            Investment.this.amount = amount;
+
+            return this;
+        }
+
+        public Builder setUser(User user) {
+            Investment.this.user = user;
+
+            return this;
+        }
+
+        public Builder setStartup(Startup startup) {
+            Investment.this.startup = startup;
+
+            return this;
+        }
+
+        public Investment build() {
+            return Investment.this;
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Investment{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", user=" + user +
+                ", startup=" + startup +
+                '}';
+    }
+
 }
