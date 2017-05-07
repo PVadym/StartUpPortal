d5c841b563ec7b177ab0a94e5367771f115be8ba
Makhinich <slav.m82@gmail.com>
2017-05-07 16:48:31 +0300

    InvestmentRepository

--- src/main/java/com/goit/startup/repository/InvestmentRepository.java
+++ src/main/java/com/goit/startup/repository/InvestmentRepository.java
@@ -8,12 +8,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 
 /**
- * TODO
+ * Interface provides a set of methods for the operation with entity {@link Investment}
+ *
+ * @author Slava Makhinich
+ * @version 1.0
  */
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 
+    /**
+     * The method finds all investments in database thad were made by entered user
+     *
+     * @param user the user whose investments we are looking for
+     * @return a collection of investments that were made by entered user
+     */
     Collection<Investment> findAllByUser(User user);
 
+    /**
+     * The method finds all investments in database thad were made into entered startup
+     *
+     * @param startup a startup, the investments we are looking for
+     * @return a collection of investments thad were made into entered startup
+     */
     Collection<Investment> findAllByStartup(Startup startup);
 
 }
