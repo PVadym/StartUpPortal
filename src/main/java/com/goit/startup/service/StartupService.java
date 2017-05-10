a0135db23e48a3854afbad520997d9f568516c80
Makhinich <slav.m82@gmail.com>
2017-05-10 22:00:05 +0300

    Refactoring

--- src/main/java/com/goit/startup/service/StartupService.java
+++ src/main/java/com/goit/startup/service/StartupService.java
@@ -1,7 +1,36 @@
 package com.goit.startup.service;
 
+import com.goit.startup.entity.Startup;
+
+import java.util.Collection;
+
 /**
- * TODO
+ * Interface provides a set of methods for the operation with products.
+ *
+ * @author Slava Makhinich
+ * @version 1.0
  */
-public interface StartupService {
+public interface StartupService extends DataService<Startup> {
+    /**
+     * The method founds startup in database by name.
+     *
+     * @param name a name of startup.
+     * @return founded startup.
+     */
+    Startup getByName(String name);
+
+    /**
+     * The method founds startup in database by name and removes it.
+     *
+     * @param name a name of startup.
+     */
+    void removeByName(String name);
+
+    /**
+     * The method finds all startups in database that contain in their name or description entered keyWord
+     *
+     * @param keyWord a word that we are trying to find in startups name and description
+     * @return a collection of startups that contain entered word in thair name or description
+     */
+    Collection<Startup> findAllByKeyWord(String keyWord);
 }
